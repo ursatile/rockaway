c059e4479508a7424fead42ffded16088b151047
Dylan Beattie <dylan@dylanbeattie.net>
2024-01-29 00:27:41 +0000

    Module 504: Let's add some photographs, and turn our artists page into something that might inspire customers to actually buy a ticket.

--- Rockaway.WebApp/Pages/Artists.cshtml.cs
+++ Rockaway.WebApp/Pages/Artists.cshtml.cs
@@ -1,10 +1,12 @@
 using Rockaway.WebApp.Data;
-using Rockaway.WebApp.Data.Entities;
+using Rockaway.WebApp.Models;
 
 namespace Rockaway.WebApp.Pages;
 
 public class ArtistsModel(RockawayDbContext db) : PageModel {
-	public IEnumerable<Artist> Artists = default!;
+	public IEnumerable<ArtistViewData> Artists = default!;
 
-	public void OnGet() => Artists = db.Artists.OrderBy(a => a.Name);
+	public void OnGet() => Artists = db.Artists
+		.Select(a => new ArtistViewData(a))
+		.ToList();
 }

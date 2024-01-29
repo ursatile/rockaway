940d53fe3520f74d47e3716a00b56798c89891e4
Dylan Beattie <dylan@dylanbeattie.net>
2024-01-29 00:26:41 +0000

    Module 301: In this module, we'll add Entity Framework (EF) Core to our project and create our first entity.

--- Rockaway.WebApp/Program.cs
+++ Rockaway.WebApp/Program.cs
@@ -1,10 +1,19 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Rockaway.WebApp.Data;
 using Rockaway.WebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
 builder.Services.AddSingleton<IStatusReporter>(new StatusReporter());
+
+var sqliteConnection = new SqliteConnection("Data Source=:memory:");
+sqliteConnection.Open();
+builder.Services.AddDbContext<RockawayDbContext>(options => options.UseSqlite(sqliteConnection));
+
 builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
 var app = builder.Build();
 
@@ -12,6 +21,12 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment()) {
 	app.UseExceptionHandler("/Error");
 	app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope()) {
+	using (var db = scope.ServiceProvider.GetService<RockawayDbContext>()!) {
+		db.Database.EnsureCreated();
+	}
 }
 
 app.UseHttpsRedirection();
